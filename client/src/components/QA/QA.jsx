ff57447ddebe11cd0ef05c4708a06eb8061c8365
Donna <donnatopia@gmail.com>
2023-01-27 16:11:20 -0800

    created questionList skeleton

--- client/src/components/QA/QA.jsx
+++ client/src/components/QA/QA.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './Search.jsx';
 import QuestionList from './QuestionList.jsx';
 import AddQuestion from './AddQuestion.jsx';
 
 const QA = () => {
+  const exampleQuestionList = {
+    product_id: 1,
+    results: [
+      {
+        "question_id": 37,
+        "question_body": "Why is this product cheaper here than other sites?",
+        "question_date": "2018-10-18T00:00:00.000Z",
+        "asker_name": "williamsmith",
+        "question_helpfulness": 4,
+        "reported": false,
+        "answers": {
+          68: {
+            "id": 68,
+            "body": "We are selling it here without any markup from the middleman!",
+            "date": "2018-08-18T00:00:00.000Z",
+            "answerer_name": "Seller",
+            "helpfulness": 4,
+            "photos": []
+          }
+        }
+      }
+    ]
+  };
+
+  const [questionList, setQuestionList] = useState(exampleQuestionList);
+
   return (
     <div>
       <h1>Questions & Answers</h1>
       <Search />
-      <QuestionList />
+      <QuestionList questionList={ questionList.results }/>
       <AddQuestion />
     </div>
   )
--- client/src/components/QA/QuestionList.jsx
+++ client/src/components/QA/QuestionList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const QuestionList = () => {
+const QuestionList = ({ questionList }) => {
   return (
     <div>
-      <h2>Question List</h2>
+      <h2>Q: </h2>
+      { questionList.map((question) => (
+        <li>{ question.question_body} </li>
+      ))}
     </div>
   )
 };
