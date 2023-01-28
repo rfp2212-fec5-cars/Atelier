21505ec138f2e89009c254c8ed7c5775fc19c507
William <wwong.w3@gmail.com>
2023-01-27 19:09:30 -0600

    Initial Skeleton for Overview

---
+++ client/src/components/Overview/ProductInformation.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+const ProductInformation = () => {
+
+
+  return (
+    <div>
+      <p>I'm Product Information</p>
+    </div>
+  );
+}
+
+export default ProductInformation;
---
+++ client/src/components/Overview/StyleSelector.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+const StyleSelector = () => {
+
+
+  return (
+    <div>
+      <p>I'm Style Selector!</p>
+    </div>
+  );
+}
+
+export default StyleSelector;
