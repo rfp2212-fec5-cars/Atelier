e5e87681850e7b755993ef13a23fb11abacc769d
Donna <donnatopia@gmail.com>
2023-01-28 18:00:50 -0800

    created getQuestion method

--- server/Controllers/QA.js
+++ server/Controllers/QA.js
@@ -2,7 +2,17 @@ const models = require('../Models/QA.js');
 
 module.exports = {
   getQuestions: (req, res) => {
+    var { product_id, page, count } = req.body
 
+    models.getQuestions(product_id, page, count)
+      .then((results) => {
+        res.status(200);
+        res.end(JSON.stringify(results.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(404);
+      })
   },
 
   getAnswers: (req, res) => {
--- server/Models/QA.js
+++ server/Models/QA.js
@@ -1,6 +1,17 @@
-module.exports = {
-  getQuestions: () => {
+const { TOKEN } = require('../config.js');
+const axios = require('axios');
 
+module.exports = {
+  getQuestions: (product_id, page, count) => {
+    var options = {
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions',
+      headers: {
+        Authorization: TOKEN
+      },
+      params: { product_id, page, count }
+    }
+
+    return axios(options);
   },
 
   getAnswers: () => {
