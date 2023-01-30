394d7d5fece0a41891062024835734fb16b8e1dc
William <wwong.w3@gmail.com>
2023-01-30 12:37:11 -0600

    Build basic Product Info from productID

--- client/src/components/Overview/ProductInformation.jsx
+++ client/src/components/Overview/ProductInformation.jsx
@@ -1,15 +1,55 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import SocialMediaButton from './Subcomponents/ProductInformation/SocialMediaButton.jsx';
 
-const ProductInformation = () => {
+
+const ProductInformation = ({productId}) => {
+
+  const [productInfo, setProductInfo] = useState({});
+  const getProductInfo = () =>{
+    let url = `/products/${productId}`;
+    axios(url)
+      .then((results) => {
+        setProductInfo(results.data);
+        // console.log('ProductInfo Results', results.data);
+      })
+      .catch((err) =>{
+        console.log('Error', err);
+      });
+  };
+
+  useEffect(()=>{
+    getProductInfo();
+  }, []);
+
+  //Star rating
+  //Product Category
+  console.log(productInfo.category);
+  //Product Title
+  console.log(productInfo.name);
+  //Price
+  console.log(productInfo.default_price);
+  //Product Overview
+  console.log(productInfo.description);
+  //Share on Social Media
+
+  let ratings = () => {
+
+  };
 
   const scrollToReviews = () => {
     document.getElementsByClassName('reviews').scrollIntoView();
   };
 
-
   return (
-    <div>
+    <div className='productInformation'>
       <p>I'm Product Information</p>
+      <div onClick={scrollToReviews}>Stars, Link to Reviews</div>
+      <div>{productInfo.category}</div>
+      <div>{productInfo.name}</div>
+      <div>${productInfo.default_price}</div>
+      <div>{productInfo.description ? productInfo.description : null }</div>
+      <div><SocialMediaButton/></div>
     </div>
   );
 };
---
+++ client/src/components/Overview/Subcomponents/ProductInformation/SocialMediaButton.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+const SocialMediaButton = (platform) => {
+
+
+
+  return (
+    <div>
+      <form>
+        <button>Empty Button For Now</button>
+      </form>
+    </div>
+  );
+};
+
+export default SocialMediaButton;
