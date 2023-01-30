feff9e4f0649da81dbf0be54b6811892efcf8700
William <wwong.w3@gmail.com>
2023-01-29 23:04:21 -0600

    Build scroll to Reviews function

--- client/src/components/Overview/ProductInformation.jsx
+++ client/src/components/Overview/ProductInformation.jsx
@@ -2,12 +2,16 @@ import React, {useState, useEffect} from 'react';
 
 const ProductInformation = () => {
 
+  const scrollToReviews = () => {
+    document.getElementsByClassName('reviews').scrollIntoView();
+  };
+
 
   return (
     <div>
       <p>I'm Product Information</p>
     </div>
   );
-}
+};
 
 export default ProductInformation;
