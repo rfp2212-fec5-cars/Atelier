7c10bb8247f6b4c5616e6f225b8dfb64a90367e9
William <wwong.w3@gmail.com>
2023-01-30 13:53:43 -0600

    render default style thumbnail

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -1,13 +1,38 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const StyleSelector = () => {
+const StyleSelector = ({productId, productStyles, setCurrentStyle}) => {
+
+  const [styles, setStyles] = useState([]);
+  const getStyles = () =>{
+    let url = `/products/${productId}/styles`;
+    axios(url)
+      .then((results) => {
+        setStyles(results.data.results);
+        setCurrentStyle(results.data.results[0]);
+        console.log('Styles Results', results.data.results);
+      });
+  };
+
+  useEffect(()=>{
+    getStyles();
+  }, []);
 
 
   return (
-    <div>
-      <p>I'm Style Selector!</p>
+    <div className = 'styleSelector'>
+      <div><b>STYLE</b></div>
+      <div>
+        {styles.map((style) => {
+          console.log(style.photos[0].thumbnail_url, 'STYLE Photos FROM MAP');
+          return (
+            <a>
+              <img src= {`${style.photos[0].thumbnail_url}`} alt='default style thumbnail'></img>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
-}
-
+};
 export default StyleSelector;
