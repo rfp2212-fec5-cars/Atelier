5910223067f7d76de3b0c23f2812928c6f19bf16
Donna <donnatopia@gmail.com>
2023-01-31 00:02:07 -0800

    increment yesCount for answers once per reload

--- client/src/components/QA/Answers/AnswerListEntry.jsx
+++ client/src/components/QA/Answers/AnswerListEntry.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const AnswerListEntry = ({ answer }) => {
+  const [alreadyLiked, setAlreadyLiked] = useState(false);
+  const [yesCount, setYesCount] = useState(answer.helpfulness);
 
   // format the date
   var date = new Date(answer.date);
   date = date.toLocaleString('default', {month: 'long'}) + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+  const handleLike = (e) => {
+    e.preventDefault();
+
+    var options = {
+      url: `/qa/answers/${answer.answer_id}/helpful`,
+      method: 'PUT'
+    }
+
+    axios(options)
+      .then(() => {
+        setAlreadyLiked(true);
+        setYesCount(yesCount + 1);
+      })
+      .catch((err) => {
+        console.log('Failed to like answer', err);
+      })
+  }
 
   return (
     <div>
       <p><b>A: </b>{ answer.body }</p>
       <p>by { answer.answerer_name === 'Seller' ? <b>Seller</b> : answer.answerer_name }, { date }</p>
       <label>Helpful? </label>
-      <button type='submit'>Yes</button>
-      { answer.helpfulness }
+      { alreadyLiked
+        ? `Yes (${yesCount})`
+        : <input type='button' onClick={e => handleLike(e)} value={`Yes (${yesCount})`}/>
+      }
       <div>
         <button type='submit'>Report</button>
       </div>
