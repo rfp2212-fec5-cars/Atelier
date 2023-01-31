4897ad8e8bd315b7c3f3677e54da0643c1c56752
Donna <donnatopia@gmail.com>
2023-01-31 00:17:27 -0800

    allowed users to like a question once per reload

--- client/src/components/QA/Questions/QuestionListEntry.jsx
+++ client/src/components/QA/Questions/QuestionListEntry.jsx
@@ -5,9 +5,16 @@ import MoreAnswers from '../Answers/MoreAnswers.jsx';
 import AddAnswer from '../Answers/AddAnswer.jsx';
 
 const QuestionListEntry = ({ question }) => {
+  // setting up states for the question details
+  const [alreadyLiked, setAlreadyLiked] = useState(false);
+  const [yesCount, setYesCount] = useState(question.question_helpfulness);
+  const [alreadyReported, setAlreadyReported] = useState(false);
+
+  // setting up states for the answer lists
   const [answerList, setAnswerList] = useState([]);
   const [displayedAnswers, setDisplayedAnswers] = useState([]);
 
+  // getting the answer list
   useEffect(() => {
     var options = {
       url: `/qa/questions/${question.question_id}/answers`,
@@ -30,13 +37,40 @@ const QuestionListEntry = ({ question }) => {
       })
   }, []);
 
+  // liking a question
+  const handleLike = (e) => {
+    e.preventDefault();
+
+    var options = {
+      url: `/qa/questions/${question.question_id}/helpful`,
+      method: 'PUT'
+    }
+
+    axios(options)
+      .then(() => {
+        setAlreadyLiked(true);
+        setYesCount(yesCount + 1);
+      })
+      .catch((err) => {
+        console.log('Failed to like question', err);
+      })
+  }
+
+  // reporting a question
+
   return (
     <div>
       <h4>Q: { question.question_body }</h4 >
       <div>
         <label>Helpful? </label>
-        <button type='submit'>Yes</button>
-        { question.question_helpfulness }
+        { alreadyLiked
+          ? `Yes (${yesCount})`
+          : <input type='button' onClick={e => handleLike(e)} value={`Yes (${yesCount})`}/>
+        }
+        { alreadyReported
+          ? 'Reported'
+          : <input type='button' onClick={e => handleReport(e)} value='Report' />
+        }
         <AddAnswer />
       </div>
       <AnswerList answers={ displayedAnswers }/>
