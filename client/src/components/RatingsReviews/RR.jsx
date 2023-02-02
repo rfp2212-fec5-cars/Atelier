ff095c7e28a2b8fd9d2e684e68fba08a89e3de69
William <wwong.w3@gmail.com>
2023-02-01 19:37:41 -0600

    merged# Please enter the commit message for your changes. Lines starting

--- client/src/components/RatingsReviews/RR.jsx
+++ client/src/components/RatingsReviews/RR.jsx
@@ -10,7 +10,10 @@ var RR = ({ productId }) => {
   const [display, setDisplay] = useState([]);
   const [total, setTotal] = useState([]);
   const [sortFilter, setSortFilter] = useState('relevant');
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
   const getReviewMeta = () => {
     let url = '/reviews/meta';
