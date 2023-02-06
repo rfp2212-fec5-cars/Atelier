1ab9d15e042dc77849d3c97d547b9c3e51376f2c
Donna <donnatopia@gmail.com>
2023-02-06 10:26:31 -0800

    standardized Modal component

--- client/src/components/QA/Answers/AddAnswer.jsx
+++ client/src/components/QA/Answers/AddAnswer.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Modal from '../../Modal/Modal.jsx';
 
 const AddAnswer = ({ product_name, question, updateAnswers, setUpdateAnswers }) => {
   const [showModal, setShowModal] = useState(false);
@@ -58,79 +59,68 @@ const AddAnswer = ({ product_name, question, updateAnswers, setUpdateAnswers }) 
     );
   };
 
-  const answerModal = (
-    <div className='qaModal'>
-      <div className='qaModalHeader'>
-        <div>
-          <h3 className='modalTitle'><span className='headingUnderline'>Submit</span> your Answer</h3>
-          <p className='modalSubtitle'><b>{ product_name }</b>: { question.question_body }</p>
-        </div>
-        <button className='closeModal' onClick={e => setShowModal(false)}>X</button>
-      </div>
-      <form onSubmit={e => postAnswer(e)}>
-        <fieldset>
-          <label htmlFor='answer_body'>Your Answer <span className='required'>*</span></label>
-          <textarea
-            name='answer_body'
-            maxLength='1000'
-            placeholder='The quality is better than expected...'
-            onChange={e => setBody(e.target.value)}
-            className='inputField'
-            onInvalid={e => e.target.setCustomValidity('You must enter an answer')}
-            onInput={e => e.target.setCustomValidity('')}
-            required >
-          </textarea>
-        </fieldset>
-        <fieldset>
-          <label htmlFor='nickname'>What is your nickname <span className='required'>*</span></label>
-          <input
-            type='text'
-            name='nickname'
-            maxLength='60'
-            placeholder='Example: jack543!'
-            onChange={e => setName(e.target.value)}
-            className='inputField'
-            onInvalid={e => e.target.setCustomValidity('You must enter your nickname')}
-            onInput={e => e.target.setCustomValidity('')}
-            required
-          />
-          <p className='disclaimer'>For privacy reasons, do not use your full name or email address</p>
-        </fieldset>
-        <fieldset>
-          <label htmlFor='email'>Your Email <span className='required'>*</span></label>
-          <input
-            type='email'
-            name='email'
-            maxLength='60'
-            placeholder='Example: jack@email.com'
-            onChange={e => setEmail(e.target.value)}
-            className='inputField'
-            onInvalid={e => e.target.setCustomValidity('The email address provided is not in the correct email format')}
-            onInput={e => e.target.setCustomValidity('')}
-            required
-          />
-          <p className='disclaimer'>For authentication reasons, you will not be emailed</p>
-        </fieldset>
-        <fieldset>
-          <label htmlFor='photos'>Upload your photos</label>
-          <div>
-            <div className='answerPhotos'>
-              { photos.map( (photo, index) => <img key={ index } src={ photo } className='answerPhoto'/> ) }
-            </div>
-            { photos.length < 5 ? <AddPhoto /> : null }
-          </div>
-        </fieldset>
-        <fieldset>
-          <button type='submit'>Submit answer</button>
-        </fieldset>
-      </form>
-    </div>
-  );
-
   return (
     <div>
       <p className='statusLink' onClick={e => setShowModal(true)} >Add Answer</p>
-      { showModal ? answerModal : null }
+      <Modal title='Submit an Answer' onClose={() => setShowModal(false)} show={ showModal } >
+        <form onSubmit={e => postAnswer(e)}>
+          <fieldset>
+            <label htmlFor='answer_body'>Your Answer <span className='required'>*</span></label>
+            <textarea
+              name='answer_body'
+              maxLength='1000'
+              placeholder='The quality is better than expected...'
+              onChange={e => setBody(e.target.value)}
+              className='form-input'
+              onInvalid={e => e.target.setCustomValidity('You must enter an answer')}
+              onInput={e => e.target.setCustomValidity('')}
+              required >
+            </textarea>
+          </fieldset>
+          <fieldset>
+            <label htmlFor='nickname'>What is your nickname <span className='required'>*</span></label>
+            <input
+              type='text'
+              name='nickname'
+              maxLength='60'
+              placeholder='Example: jack543!'
+              onChange={e => setName(e.target.value)}
+              className='form-input'
+              onInvalid={e => e.target.setCustomValidity('You must enter your nickname')}
+              onInput={e => e.target.setCustomValidity('')}
+              required
+            />
+            <p className='disclaimer'>For privacy reasons, do not use your full name or email address</p>
+          </fieldset>
+          <fieldset>
+            <label htmlFor='email'>Your Email <span className='required'>*</span></label>
+            <input
+              type='email'
+              name='email'
+              maxLength='60'
+              placeholder='Example: jack@email.com'
+              onChange={e => setEmail(e.target.value)}
+              className='form-input'
+              onInvalid={e => e.target.setCustomValidity('The email address provided is not in the correct email format')}
+              onInput={e => e.target.setCustomValidity('')}
+              required
+            />
+            <p className='disclaimer'>For authentication reasons, you will not be emailed</p>
+          </fieldset>
+          <fieldset>
+            <label htmlFor='photos'>Upload your photos</label>
+            <div>
+              <div className='answerPhotos'>
+                { photos.map( (photo, index) => <img key={ index } src={ photo } className='answerPhoto'/> ) }
+              </div>
+              { photos.length < 5 ? <AddPhoto /> : null }
+            </div>
+          </fieldset>
+          <fieldset>
+            <button type='submit'>Submit answer</button>
+          </fieldset>
+        </form>
+      </Modal>
     </div>
   );
 }
--- client/src/components/QA/Questions/AddQuestion.jsx
+++ client/src/components/QA/Questions/AddQuestion.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Modal from '../../Modal/Modal.jsx';
 
 const AddQuestion = ({ product_id, product_name, updateQuestions, setUpdateQuestions }) => {
   const [showModal, setShowModal] = useState(false);
@@ -27,69 +28,59 @@ const AddQuestion = ({ product_id, product_name, updateQuestions, setUpdateQuest
       })
   }
 
-  const questionModal = (
-    <div className='qaModal'>
-      <div className='qaModalHeader'>
-        <h3>Ask Your Question</h3>
-        <button className='closeModal' onClick={e => setShowModal(false) }>X</button>
-      </div>
-      <h4 className='modalSubtitle'>About the { product_name }</h4>
-      <form onSubmit={e => postQuestion(e)}>
-        <fieldset>
-          <label htmlFor='question_body'>Your Question <span className='required'>*</span></label>
-          <textarea
-            name='question_body'
-            maxLength='1000'
-            placeholder='How is the quality of...'
-            onChange={e => setBody(e.target.value)}
-            className='inputField'
-            onInvalid={e => e.target.setCustomValidity('You must enter a question')}
-            onInput={e => e.target.setCustomValidity('')}
-            required >
-          </textarea>
-        </fieldset>
-        <fieldset>
-          <label htmlFor='nickname'>What is your nickname <span className='required'>*</span></label>
-          <input
-            type='text'
-            name='nickname'
-            maxLength='60'
-            placeholder='Example: jackson11!'
-            onChange={e => setName(e.target.value)}
-            className='inputField'
-            onInvalid={e => e.target.setCustomValidity('You must enter your nickname')}
-            onInput={e => e.target.setCustomValidity('')}
-            required
-          />
-          <p className='disclaimer'>For privacy reasons, do not use your full name or email address</p>
-        </fieldset>
-        <fieldset>
-          <label htmlFor='email'>Your Email <span className='required'>*</span></label>
-          <input
-            type='email'
-            name='email'
-            maxLength='60'
-            placeholder='Why did you like the product or not?'
-            onChange={e => setEmail(e.target.value)}
-            className='inputField'
-            onInvalid={e => e.target.setCustomValidity('The email address provided is not in the correct email format')}
-            onInput={e => e.target.setCustomValidity('')}
-            required
-          />
-          <p className='disclaimer'>For authentication reasons, you will not be emailed</p>
-        </fieldset>
-        <fieldset>
-          <button type='submit'>Submit Question</button>
-        </fieldset>
-      </form>
-    </div>
-  )
-
-
   return (
     <div>
       <button onClick={e => setShowModal(true)}>Ask Your Question</button>
-      { showModal ? questionModal : null }
+      <Modal title='Ask Your Question' onClose={() => setShowModal(false)} show={ showModal }>
+        <form onSubmit={e => postQuestion(e)}>
+          <fieldset>
+            <label htmlFor='question_body'>Your Question <span className='required'>*</span></label>
+            <textarea
+              name='question_body'
+              maxLength='1000'
+              placeholder='How is the quality of...'
+              onChange={e => setBody(e.target.value)}
+              className='form-input'
+              onInvalid={e => e.target.setCustomValidity('You must enter a question')}
+              onInput={e => e.target.setCustomValidity('')}
+              required >
+            </textarea>
+          </fieldset>
+          <fieldset>
+            <label htmlFor='nickname'>What is your nickname <span className='required'>*</span></label>
+            <input
+              type='text'
+              name='nickname'
+              maxLength='60'
+              placeholder='Example: jackson11!'
+              onChange={e => setName(e.target.value)}
+              className='form-input'
+              onInvalid={e => e.target.setCustomValidity('You must enter your nickname')}
+              onInput={e => e.target.setCustomValidity('')}
+              required
+            />
+            <p className='disclaimer'>For privacy reasons, do not use your full name or email address</p>
+          </fieldset>
+          <fieldset>
+            <label htmlFor='email'>Your Email <span className='required'>*</span></label>
+            <input
+              type='email'
+              name='email'
+              maxLength='60'
+              placeholder='Why did you like the product or not?'
+              onChange={e => setEmail(e.target.value)}
+              className='form-input'
+              onInvalid={e => e.target.setCustomValidity('The email address provided is not in the correct email format')}
+              onInput={e => e.target.setCustomValidity('')}
+              required
+            />
+            <p className='disclaimer'>For authentication reasons, you will not be emailed</p>
+          </fieldset>
+          <fieldset>
+            <button type='submit'>Submit Question</button>
+          </fieldset>
+        </form>
+      </Modal>
     </div>
   )
 };
