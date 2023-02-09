883fadb3125b1aaa36491711e18decf57e8bab70
Donna <donnatopia@gmail.com>
2023-02-09 12:29:28 -0800

    logInteraction for like/report answer

--- client/src/components/QA/Answers/AnswerList.jsx
+++ client/src/components/QA/Answers/AnswerList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import AnswerListEntry from './AnswerListEntry.jsx';
 
-const AnswerList = ({ answers }) => {
+const AnswerList = ({ answers, logInteraction }) => {
   return (
     <div className='answerList'>
       { answers.map((answer, index) => (
-        <AnswerListEntry key={ answer.answer_id } answer={ answer } index={ index + 1 }/>
+        <AnswerListEntry key={ answer.answer_id } answer={ answer } index={ index + 1 } logInteraction={ logInteraction }/>
       ))}
     </div>
   );
